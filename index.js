9a3e1fb1139e5e7cdd52afc05289a048c49a903c
ShimulChandraDas <msshimul99@gmail.com>
2022-05-06 02:32:25 +0600

    delete api ready

--- index.js
+++ index.js
@@ -46,6 +46,14 @@ async function run() {
             res.send(result);
         })
 
+        //DELETE
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
 

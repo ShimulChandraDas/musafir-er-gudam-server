d547ba529832dfdb348893211891f119029029e0
ShimulChandraDas <msshimul99@gmail.com>
2022-05-08 03:22:07 +0600

    delevered

--- index.js
+++ index.js
@@ -60,15 +60,15 @@ async function run() {
             const result = await productCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         })
-        DELiVered
+        // DeliVered
         app.put('/product/:id', async (req, res) => {
             const id = req.params.id;
-            const updatedQuantity = req.body;
+            const delivered = req.body;
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
             const updatedDoc = {
                 $set: {
-                    quantity: updatedQuantity.quantity
+                    quantity: delivered.quantity
                 }
             };
             const result = await productCollection.updateOne(filter, updatedDoc, options);
